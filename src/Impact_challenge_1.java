3eecfd647ab9b411fd61f750786a21050f4c2900
Sisonke77 <69693949+Sisonke77@users.noreply.github.com>
2022-01-29 00:06:16 +0200

    Impact_challenge_1 initial commit, Tests don't pass

---
+++ src/Impact_challenge_1.java
@@ -0,0 +1,16 @@
+import com.example.numberrangesummarizer.Summarize;
+
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Impact_challenge_1 {
+    static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        String numbers = myObj.nextLine();
+        myObj.close();
+        Summarize Str_summary = new Summarize();
+        Collection<Integer> numbers_splinted = Str_summary.collect(numbers);
+        String summary = Str_summary.summarizeCollection(numbers_splinted);
+        System.out.println(summary);
+    }
+}
---
+++ src/com/example/numberrangesummarizer/Summarize.java
@@ -0,0 +1,16 @@
+package com.example.numberrangesummarizer;
+
+import java.util.Collection;
+
+public class Summarize implements NumberRangeSummarizer{
+
+    @Override
+    public Collection<Integer> collect(String input) {
+        return null;
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return null;
+    }
+}
